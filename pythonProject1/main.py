e06edb543fb2c2097413c12ec454e04a40a1d1b0
251160 <251160@edu.p.lodz.pl>
2025-05-12 18:39:01 +0200

    add primitive player movement using arrow keys

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -16,10 +16,14 @@ playerImg = pygame.image.load('idle.gif')
 playerImg = pygame.transform.scale(playerImg, (80, 80))
 PLAYER_SIZE_X = 80
 PLAYER_SIZE_Y = 80
+PLAYER_MOVEMENT_SPEED = 0.05
 
 #set player position
 playerX = 400
 playerY = 400
+#for movement
+playerX_change = 0
+playerY_change = 0
 
 def player():
     #draw player sprite
@@ -32,9 +36,26 @@ while running:
         #event handler (the top right X button is pressed)
         if event.type == pygame.QUIT:
             running = False
+        #keyboard events
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                playerY_change = PLAYER_MOVEMENT_SPEED
+            if event.key == pygame.K_UP:
+                playerY_change = -PLAYER_MOVEMENT_SPEED
+            if event.key == pygame.K_LEFT:
+                playerX_change = -PLAYER_MOVEMENT_SPEED
+            if event.key == pygame.K_RIGHT:
+                playerX_change = PLAYER_MOVEMENT_SPEED
+        #stop moving if key is no longer pressed
+        if event.type == pygame.KEYUP:
+            playerX_change = 0
+            playerY_change = 0
 
     #change screen color (RGB format) // to see changes use update method
     screen.fill((200,0,24))
-    #player needs to be undernearth screen fill function as to not get obstructed
+    #move player
+    playerY += playerY_change
+    playerX += playerX_change
+    #player draw function needs to be called after screen fill function as to not get obstructed
     player()
     pygame.display.update()
