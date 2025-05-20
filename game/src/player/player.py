b8bca640bce4b5026cfd7b00043c237570cdb52e
Mateusz Jędraszczyk <246157@edu.p.lodz.pl>
2025-05-20 02:00:15 +0200

    Add directional walking animations

--- game/src/player/player.py
+++ game/src/player/player.py
@@ -6,10 +6,24 @@ class Player:
     self.pos_x = 400
     self.pos_y = 400
     self.movement_speed = 2
+    self.current_animation = filename
 
-  def draw(self, screen, screen_x, screen_y):
-    screen.blit(self.player_img_info.player_image, (screen_y / 2 - self.player_img_info.scale_size_y / 2, screen_x / 2 - self.player_img_info.scale_size_x / 2))
+  def draw(self, screen, screen_x, screen_y, dt):
+    # Get the current animation frame based on elapsed time (dt)
+    frame = self.player_img_info.get_current_frame(dt)
+    # Draw the current frame centered on the screen
+    screen.blit(frame, (screen_y / 2 - self.player_img_info.scale_size_y / 2,
+                        screen_x / 2 - self.player_img_info.scale_size_x / 2))
 
   def update_position(self, x, y):
     self.pos_x += x
     self.pos_y += y
+
+  def set_animation(self, filename):
+    # If the requested animation is already active, do nothing
+    if self.current_animation == filename:
+        return
+    # Otherwise, update the current animation and reload frames
+    self.current_animation = filename
+    self.player_img_info = PlayerImageInfo(filename)
+
--- game/src/player/player_image_info.py
+++ game/src/player/player_image_info.py
@@ -1,12 +1,57 @@
+from PIL import Image
 import pygame
 
 class PlayerImageInfo:
+    def __init__(self, gif_path):
+        # Load all frames from the GIF file
+        self.frames = self.load_gif_frames(gif_path)
+        self.frame_index = 0  # Index of the current frame
+        self.animation_speed = 0.25  # Lower value means faster animation
+        self.time_since_last_frame = 0.0  # Time since last frame update
 
-  def __init__(self, filename):
-    self.filename = filename
+        # Set final scaled width and height (from the first frame)
+        self.scale_size_x = self.frames[0].get_width()
+        self.scale_size_y = self.frames[0].get_height()
 
-    self.scale_size_x = 80
-    self.scale_size_y = 80
+    def load_gif_frames(self, gif_path):
+        # Open the GIF using PIL
+        pil_image = Image.open(gif_path)
+        frames = []
 
-    self.player_image = pygame.image.load(filename)
-    self.player_image = pygame.transform.scale(self.player_image, (self.scale_size_x, self.scale_size_y))
+        scale_x = 80  # Target width
+        scale_y = 80  # Target height
+
+        try:
+            while True:
+                # Convert the current frame to RGBA format
+                frame = pil_image.convert('RGBA')
+                mode = frame.mode
+                size = frame.size
+                data = frame.tobytes()
+                # Convert PIL image data to Pygame surface
+                pygame_image = pygame.image.fromstring(data, size, mode)
+                # Scale the frame to the desired size
+                pygame_image = pygame.transform.scale(pygame_image, (scale_x, scale_y))
+                # Add the scaled frame to the list
+                frames.append(pygame_image)
+                # Move to the next frame
+                pil_image.seek(pil_image.tell() + 1)
+        except EOFError:
+            pass
+
+        # Store scaled size (used for centering)
+        self.scale_size_x = scale_x
+        self.scale_size_y = scale_y
+
+        return frames
+
+    def get_current_frame(self, dt):
+        # Increase time since last frame by the delta time
+        self.time_since_last_frame += dt
+        # If it's time to change the frame
+        if self.time_since_last_frame >= self.animation_speed:
+            # Move to the next frame (loop back to start if needed)
+            self.frame_index = (self.frame_index + 1) % len(self.frames)
+            self.time_since_last_frame = 0
+        # Return the current frame surface
+        return self.frames[self.frame_index]
