e595ab0ff90c8d4321ab52394334e51db58f3a69
Mateusz Jędraszczyk <246157@edu.p.lodz.pl>
2025-06-26 00:00:51 +0200

    better look for ui

--- game/src/ui/UI.py
+++ game/src/ui/UI.py
@@ -9,28 +9,42 @@ class UI:
         self.font_path = "assets/menu/font.ttf"
 
         self.font_main = pygame.font.Font(self.font_path, 14)
-        self.font_nick = pygame.font.Font(self.font_path, 16)
-
+        self.font_large = pygame.font.Font(self.font_path, 20)
+        self.font_nick = pygame.font.Font(self.font_path, 18)
 
         self.text_color = (255, 255, 255)
 
+        # Współczynnik skalowania dla ikon
+        icon_scale_factor = 2.25
+        ects_icon_scale_factor = 1.5
+
+        # Załadowanie i opcjonalne przeskalowanie ikon
         self.icons = {
-            'ects': pygame.image.load('assets/tiles/icons/ects.png').convert_alpha(),
+            'ects': pygame.transform.scale_by(
+                pygame.image.load('assets/tiles/icons/ects.png').convert_alpha(),
+                ects_icon_scale_factor),
             'time': pygame.image.load('assets/tiles/icons/time.png').convert_alpha(),
             'heart_full': pygame.image.load('assets/tiles/icons/sprite_4.png').convert_alpha(),
             'heart_empty': pygame.image.load('assets/tiles/icons/sprite_3.png').convert_alpha(),
             'quit': pygame.image.load('assets/tiles/icons/quit.png').convert_alpha(),
-            'options': pygame.image.load('assets/tiles/icons/gear.png').convert_alpha(),
-            'pause': pygame.image.load('assets/tiles/icons/icons1.png').convert_alpha(),
-            'help': pygame.image.load('assets/tiles/icons/icons0.png').convert_alpha()
+            # Przeskalowanie ikon na prawym panelu
+            'options': pygame.transform.scale_by(
+                pygame.image.load('assets/tiles/icons/gear.png').convert_alpha(),
+                icon_scale_factor),
+            'pause': pygame.transform.scale_by(
+                pygame.image.load('assets/tiles/icons/icons1.png').convert_alpha(),
+                icon_scale_factor),
+            'help': pygame.transform.scale_by(
+                pygame.image.load('assets/tiles/icons/icons0.png').convert_alpha(),
+                icon_scale_factor)
         }
 
         # SKALOWANIE RAMEK (szerokość i wysokość)
         end_cap_scale_factor = 1.5
 
-        left_border_img = pygame.image.load('assets/tiles/borders/borders_0.png').convert_alpha()
-        right_border_img = pygame.image.load('assets/tiles/borders/borders_1.png').convert_alpha()
-        middle_border_img = pygame.image.load('assets/tiles/borders/border2_3.png').convert_alpha()
+        left_border_img = pygame.image.load('assets/tiles/borders/left_border.png').convert_alpha()
+        right_border_img = pygame.image.load('assets/tiles/borders/right_border.png').convert_alpha()
+        middle_border_img = pygame.image.load('assets/tiles/borders/middle_border.png').convert_alpha()
 
         original_left_height = left_border_img.get_height()
         original_left_width = left_border_img.get_width()
@@ -43,130 +57,160 @@ class UI:
         new_left_height = int(original_left_height * end_cap_scale_factor)
         new_right_width = int(original_right_width * end_cap_scale_factor)
         new_right_height = int(original_right_height * end_cap_scale_factor)
-
         new_middle_height = int(original_middle_height * end_cap_scale_factor)
+
+        self.bar_height = new_middle_height - 8
 
         self.borders = {
             'left': pygame.transform.scale(left_border_img, (new_left_width, new_left_height)),
-            'middle': pygame.transform.scale(middle_border_img, (middle_border_img.get_width(), new_middle_height)),
+            'middle_original': pygame.transform.scale(middle_border_img,
+                                                      (middle_border_img.get_width(), self.bar_height)),
             'right': pygame.transform.scale(right_border_img, (new_right_width, new_right_height))
         }
 
-        # Wysokość panelu jest zdeterminowana przez wysokość jego części
-        self.bar_height = self.borders[
-            'middle'].get_height()
-
     def _draw_top_bar(self, x, y, width):
-        """Rysuje jeden, długi panel na górze ekranu."""
-
-        # Lewe zakończenie
         self.screen.blit(self.borders['left'], (x, y))
 
-        # Środek
-        middle_width = self.borders['middle'].get_width()
-        current_x = x + self.borders['left'].get_width()
-        if middle_width == 0:
-            print("Ostrzeżenie: Szerokość środkowej ramki wynosi 0. Nie będzie rysowana.")
-        else:
-            while current_x < x + width - self.borders['right'].get_width():
-                self.screen.blit(self.borders['middle'], (current_x, y))
-                current_x += middle_width
-                if current_x + self.borders['right'].get_width() > x + width:
-                    break
+        left_end_x = x + self.borders['left'].get_width()
+        right_start_x = x + width - self.borders['right'].get_width()
+        stretch_width = max(0, right_start_x - left_end_x)
+        stretched_middle_img = pygame.transform.scale(
+            self.borders['middle_original'],
+            (stretch_width, self.bar_height)
+        )
 
+        self.screen.blit(stretched_middle_img, (left_end_x, y))
         self.screen.blit(self.borders['right'], (x + width - self.borders['right'].get_width(), y))
 
         return pygame.Rect(x, y, width, self.bar_height)
 
     def draw(self, player_data, time_left, objective_text):
-        """Główna metoda rysująca UI, dostosowana do zamienionych wymiarów."""
-
         actual_screen_width = Constants.WINDOW_HEIGHT
         bar_rect = self._draw_top_bar(0, 0, actual_screen_width)
 
-        # Marginesy WEWNĄTRZ panelu
-        padding_h = 25
-        padding_v = 8
+        # SEKCJA LEWA (Nick i Życia)
+        left_section_area_width = self.borders['left'].get_width()
+        left_section_start_x = bar_rect.left
 
-        # SEKCJA LEWA (ECTS, Czas, Stamina)
-        left_section_x = bar_rect.left + padding_h
+        nick_surf = self.font_large.render(player_data.player_name, True, self.text_color)
+        heart_width = self.icons['heart_full'].get_width()
+        hearts_spacing = 5
+        hearts_height = heart_width
 
-        offset_down_left_panel = 15
+        # Całkowita wysokość bloku z nickiem i sercami (nick + przerwa + serca)
+        total_left_elements_height = nick_surf.get_height() + hearts_spacing + hearts_height
 
-        # ECTS (górna linia)
-        ects_y = bar_rect.top + padding_v + offset_down_left_panel
-        self.screen.blit(self.icons['ects'], (left_section_x, ects_y))
-        ects_text = self.font_main.render(f"{player_data.ects} ECTS", True, self.text_color)
-        self.screen.blit(ects_text, (left_section_x + self.icons['ects'].get_width() + 5, ects_y + 1))
+        # Wyśrodkuj pionowo: start Y = góra paska + (wysokość paska - wysokość bloku) / 2
+        start_y_left_section = bar_rect.top + (self.bar_height - total_left_elements_height) // 2
 
-        # Czas (środkowa linia)
-        time_y = bar_rect.top + padding_v + 22 + offset_down_left_panel
-        self.screen.blit(self.icons['time'], (left_section_x, time_y))
-        minutes = int(time_left) // 60
-        seconds = int(time_left) % 60
-        time_text = self.font_main.render(f"{minutes:02d}:{seconds:02d}", True, self.text_color)
-        self.screen.blit(time_text, (left_section_x + self.icons['time'].get_width() + 5, time_y + 1))
+        # Wyśrodkuj nick poziomo w obrębie lewej ramki
+        nick_x = left_section_start_x + (left_section_area_width - nick_surf.get_width()) // 2 - 10
+        nick_y = start_y_left_section - 30
+        self.screen.blit(nick_surf, (nick_x, nick_y))
 
-        # Stamina (dolna linia)
-        stamina_y = bar_rect.top + padding_v + 44 + offset_down_left_panel
-        stamina_rect = pygame.Rect(left_section_x, stamina_y, 140, 12)
-        pygame.draw.rect(self.screen, (40, 40, 40), stamina_rect, border_radius=3)
+        # Serca wyśrodkuj poziomo w tej samej szerokości
+        hearts_total_width = player_data.max_lives * heart_width + (player_data.max_lives - 1) * hearts_spacing
+        hearts_start_x = left_section_start_x + (left_section_area_width - hearts_total_width) // 2 - 10
+        hearts_y = nick_y + nick_surf.get_height() + hearts_spacing
+
+        for i in range(player_data.max_lives):
+            heart_x = hearts_start_x + i * (heart_width + hearts_spacing)
+            heart_icon = self.icons['heart_full'] if i < player_data.lives else self.icons['heart_empty']
+            self.screen.blit(heart_icon, (heart_x, hearts_y))
+
+        # SEKCJA ŚRODKOWA (Objective, ECTS, Stamina, Time)
+        middle_section_start_x = bar_rect.left + self.borders['left'].get_width()
+        middle_section_end_x = bar_rect.right - self.borders['right'].get_width()
+        middle_section_area_width = middle_section_end_x - middle_section_start_x
+
+        # Odstępy między elementami pionowo w środkowej sekcji
+        element_spacing_middle = 5
+
+        # Przygotuj powierzchnie tekstowe i uzyskaj ich wymiary
+        objective_surf = self.font_large.render(objective_text, True, self.text_color)
+        ects_text_surf = self.font_large.render(f"{player_data.ects} ECTS", True, self.text_color)
+        time_text_surf = self.font_main.render(f"{int(time_left) // 60:02d}:{int(time_left) % 60:02d}", True,
+                                               self.text_color)
+
+        top_margin_objective = 20
+        current_y_middle = bar_rect.top + top_margin_objective
+
+        # 1. Cel (Objective) – NA GÓRZE
+        objective_x = middle_section_start_x + (middle_section_area_width - objective_surf.get_width()) // 2
+        self.screen.blit(objective_surf, (objective_x, current_y_middle))
+
+        current_y_middle += objective_surf.get_height() + element_spacing_middle
+
+        # 2. ECTS + ikona
+        ects_offset_y = 14
+        ects_block_width = self.icons['ects'].get_width() + 5 + ects_text_surf.get_width()
+        ects_block_x = middle_section_start_x + (middle_section_area_width - ects_block_width) // 2
+        self.screen.blit(self.icons['ects'], (
+            ects_block_x,
+            current_y_middle + (ects_text_surf.get_height() - self.icons['ects'].get_height()) / 2 + ects_offset_y))
+        self.screen.blit(ects_text_surf, (
+            ects_block_x + self.icons['ects'].get_width() + 5, current_y_middle + ects_offset_y))
+
+        current_y_middle += max(ects_text_surf.get_height(), self.icons['ects'].get_height()) + element_spacing_middle
+
+        # 3. Pasek staminy
+        stamina_bar_height = 16
+        stamina_bar_margin_bottom = 60
+        stamina_bar_width = int(middle_section_area_width * 0.75)
+
+        stamina_rect_x = middle_section_start_x + (middle_section_area_width - stamina_bar_width) // 2
+        stamina_rect_y = bar_rect.bottom - stamina_bar_margin_bottom - stamina_bar_height
+
+        stamina_rect = pygame.Rect(stamina_rect_x, stamina_rect_y, stamina_bar_width, stamina_bar_height)
+        pygame.draw.rect(self.screen, (20, 20, 20), stamina_rect, border_radius=3)
+        pygame.draw.rect(self.screen, (80, 80, 80), stamina_rect, width=2, border_radius=3)
+
         stamina_ratio = player_data.stamina / player_data.max_stamina
         fill_width = (stamina_rect.width - 4) * stamina_ratio
         fill_rect = pygame.Rect(stamina_rect.x + 2, stamina_rect.y + 2, fill_width, stamina_rect.height - 4)
         pygame.draw.rect(self.screen, (50, 200, 50), fill_rect, border_radius=3)
-        pygame.draw.rect(self.screen, (200, 200, 200), stamina_rect, 1, border_radius=3)
 
-        # SEKCJA ŚRODKOWA (Życia i Nick)
-        nick_surf = self.font_nick.render(player_data.player_name, True, self.text_color)
-        nick_rect = nick_surf.get_rect()
+        # 4. Czas
+        bottom_margin_time = 25
+        time_block_width = time_text_surf.get_width() + self.icons['time'].get_width() + 5
+        time_block_x = middle_section_start_x + (middle_section_area_width - time_block_width) // 2
 
-        heart_width = self.icons['heart_full'].get_width()
-        hearts_width = player_data.max_lives * heart_width + (player_data.max_lives - 1) * 5
+        time_y = bar_rect.bottom - bottom_margin_time - max(time_text_surf.get_height(),
+                                                            self.icons['time'].get_height())
 
-        # Ustalenie wspólnego środka pionowego dla sekcji środkowej
-        center_y_for_middle_section = bar_rect.centery
-
-        offset_up_middle_panel = 10
-
-        # Obliczanie pozycji startowej X dla całej sekcji środkowej (pozostaje bez zmian)
-        total_middle_elements_width = nick_rect.width + 20 + hearts_width
-        start_x_for_middle_section = bar_rect.centerx - (total_middle_elements_width // 2)
-
-        # Ustawienie pozycji nicku
-        nick_rect.topleft = (
-        start_x_for_middle_section, center_y_for_middle_section - nick_rect.height // 2 - offset_up_middle_panel)
-        self.screen.blit(nick_surf, nick_rect)
-
-        # Ustawienie pozycji serc
-        hearts_start_x = nick_rect.right + 20
-        for i in range(player_data.max_lives):
-            heart_x = hearts_start_x + i * (heart_width + 5)
-            self.screen.blit(
-                self.icons['heart_full'] if i < player_data.lives else self.icons['heart_empty'],
-                (heart_x, center_y_for_middle_section - heart_width // 2 - offset_up_middle_panel)
-            )
-
-
+        self.screen.blit(self.icons['time'],
+                         (time_block_x, time_y + (time_text_surf.get_height() - self.icons['time'].get_height()) / 2))
+        self.screen.blit(time_text_surf, (time_block_x + self.icons['time'].get_width() + 5, time_y))
 
         # SEKCJA PRAWA (Pozostałe ikony)
-        icon_y = bar_rect.centery - self.icons['quit'].get_height() // 2
-        icon_margin = 8
+        right_section_start_x = bar_rect.right - self.borders['right'].get_width()
+        right_section_area_width = self.borders['right'].get_width()
 
-        quit_x = bar_rect.right - padding_h - self.icons['quit'].get_width()
-        self.screen.blit(self.icons['quit'], (quit_x, icon_y))
+        icon_margin = 8  # Odstęp między ikonami
 
-        options_x = quit_x - icon_margin - self.icons['options'].get_width()
-        self.screen.blit(self.icons['options'], (options_x, icon_y))
+        total_right_icons_width = (
+                self.icons['options'].get_width() + icon_margin +
+                self.icons['pause'].get_width() + icon_margin +
+                self.icons['help'].get_width()
+        )
 
-        pause_x = options_x - icon_margin - self.icons['pause'].get_width()
-        self.screen.blit(self.icons['pause'], (pause_x, icon_y))
+        # Wyśrodkuj cały blok ikon poziomo w prawej sekcji
+        start_x_right_section = right_section_start_x + (right_section_area_width - total_right_icons_width) // 2
 
-        help_x = pause_x - icon_margin - self.icons['help'].get_width()
-        self.screen.blit(self.icons['help'], (help_x, icon_y))
+        current_x_right = start_x_right_section + 15
+        base_icon_y = bar_rect.centery + (-20)
 
-        # DODANIE OBJECTIVE TEXT
-        objective_surf = self.font_main.render(objective_text, True, self.text_color)
-        objective_rect = objective_surf.get_rect(
-            center=(bar_rect.centerx, bar_rect.bottom - padding_v - objective_surf.get_height() // 2))
-        self.screen.blit(objective_surf, objective_rect)
+        # Ikona 'help'
+        help_icon_y = base_icon_y - self.icons['help'].get_height() // 2
+        self.screen.blit(self.icons['help'], (current_x_right, help_icon_y))
+        current_x_right += self.icons['help'].get_width() + icon_margin
+
+        # Ikona 'pause'
+        pause_icon_y = base_icon_y - self.icons['pause'].get_height() // 2
+        self.screen.blit(self.icons['pause'], (current_x_right, pause_icon_y))
+        current_x_right += self.icons['pause'].get_width() + icon_margin
+
+        # Ikona 'options'
+        options_icon_y = base_icon_y - self.icons['options'].get_height() // 2
+        self.screen.blit(self.icons['options'], (current_x_right, options_icon_y))
+        current_x_right += self.icons['options'].get_width() + icon_margin
