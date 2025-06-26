21c85a585675ff2d902e3736ad85a6db9d7de88d
251160 <251160@edu.p.lodz.pl>
2025-06-26 19:14:51 +0200

    add objects and borders

---
+++ game/assets/map_data/water.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="water" tilewidth="16" tileheight="16" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image source="../tiles/water/water_1.png" width="16" height="16"/>
+ </tile>
+ <tile id="2">
+  <image source="../tiles/water/water_2.png" width="16" height="16"/>
+ </tile>
+</tileset>
